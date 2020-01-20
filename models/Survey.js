8d0fa263bad456a25d8bd0831e3fc81fabaa07f3
Daniel Lee <dan.dh.lee@gmail.com>
2020-01-19 16:02:58 -0800

    Add survey routes and Mailer

--- models/Survey.js
+++ models/Survey.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose');
 const { Schema } = mongoose;
-const RecipientSchema = require('./RecipientSchema');
+const RecipientSchema = require('./Recipient');
 
 const surveySchema = new Schema({
     title: String,
