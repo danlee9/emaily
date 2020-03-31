e69d100cf16c1e89b809f6edb2d2fa44871eb749
Daniel Lee <dan.dh.lee@gmail.com>
2020-03-31 00:34:03 -0700

    Round out validate emails utility function

--- client/src/components/surveys/SurveyForm.js
+++ client/src/components/surveys/SurveyForm.js
@@ -3,6 +3,7 @@ import React from 'react';
 import { reduxForm, Field } from "redux-form";
 import { Link } from "react-router-dom";
 import SurveyField from './SurveyField';
+import validateEmails from '../../utils/validateEmails';
 
 const FIELDS = [
     { label: 'Survey Title', name: 'title'},
@@ -35,6 +36,8 @@ class SurveyForm extends React.Component {
 
 function validate(values) {
     const errors = {};
+
+    errors.emails = validateEmails(values.emails || '');
 
     _.each(FIELDS, ({ name }) => {
         if (!values[name]) {
