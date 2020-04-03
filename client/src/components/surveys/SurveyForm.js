cce4477a9970eaf220ea8d6d623bacdd2259bbcd
Daniel Lee <dan.dh.lee@gmail.com>
2020-04-02 22:44:43 -0700

    Add survey review form

--- client/src/components/surveys/SurveyForm.js
+++ client/src/components/surveys/SurveyForm.js
@@ -24,7 +24,7 @@ class SurveyForm extends React.Component {
     render() {
         return (
             <div>
-                <form onSubmit={this.props.handleSubmit(values => console.log(values))}>
+                <form onSubmit={this.props.handleSubmit(this.props.onSurveySubmit)}>
                     {this.renderFields()}
                     <Link to="/surveys" className="red btn-flat white-text">Cancel</Link>
                     <button type="submit" className="teal btn-flat right white-text">Next <i className="material-icons right">done</i></button>
